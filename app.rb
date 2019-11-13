da28346bf929959034c1aecf541533ee64929164
jacquibushfi <jacqui@stainedglassofmckinney.com>
2019-11-13 15:28:32 -0600

    Done.

--- app.rb
+++ app.rb
@@ -10,12 +10,9 @@ class App < Sinatra::Base
     erb :create_puppy
   end
 
-  post "/display_puppy" do
+  post "/puppy" do
     @puppy = Puppy.new(params['name'], params['breed'], params['months_old'])
     erb :display_puppy
-
   end
-
-learn 
 
 end
